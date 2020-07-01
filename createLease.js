ffa102454ae5cd12144de692db0b435620745301
Ivan Popov <ivantsar@gmail.com>
2020-07-01 19:02:17 -0400

    set up inquirer structure and lease formating

--- createLease.js
+++ createLease.js
@@ -4,6 +4,7 @@ function createLease(d) {
 return `
 RENTAL AGREEMENT
 
+
 1) PARTIES: This agreement is entered into on this date ${date} between the following parties RESIDENT(S): ${d.part1} ${d.part2} and MANAGER: ${d.landlordName}. Resident agrees to rent from the owner of the premises at the following location subject to the terms and conditions of this agreement.
 
 2) SUBJECT PROPERTY: ${d.subj}
